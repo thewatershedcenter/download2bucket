e7920ffc500fbd57cf3c7da800ab6992b1e93672
michael <michaelhuggins@protonmail.com>
2021-09-14 11:20:11 -0700

    modified help

--- start.sh
+++ start.sh
@@ -12,22 +12,26 @@ while (( "$#" )); do
             echo "     Copies them to gcloud bucket"
             echo "        "
             echo "     flags:"
+            echo "        "
             echo "        - h - display help"
             echo "        "
             echo "        - r - if set local files will be deleted after being colpied to the bucket"
             echo "        "
             echo "     positional arguments:"
+            echo "        "
             echo "        [key_file] - gcloud service account key json file, see"
-            echo "        https://cloud.google.com/docs/authentication/getting-started"
-
+            echo "        https://cloud.google.com/docs/authentication/getting-started,"
+            echo "        or use gcloud iam service-accounts keys create"
+            echo "        " 
             echo "        [list_of_urls] - list of urls to be downloaded, one per line."
             echo "        "
             echo "        [output_dir] - local directory that will be mounted by the"
             echo "        container and used to store downloaded files"
+            echo "        "
             echo "        [bucket] - name of gcloud bucket to which files should be transfered"
             echo "        "
-            echo "        [bucket_dir] - name of directory which will be created in bucket to"
-            echo "        store files. (It is my understanding that google buckets do not"
+            echo "        [bucket_dir] - name of directory which will be created (if it does not exist)"
+            echo "        in bucket to store files. (It is my understanding that google buckets do not"
             echo "        actually have directories, they are flat, but the files will appear"
             echo "        in the bucket with names as though they were in directories)"
             shift
@@ -42,22 +46,18 @@ while (( "$#" )); do
     esac
 done
 
-
-
-
 KEYJSON=$1
 URLS=$2
 OUTDIR=$3
 BUCKET=$4
 BUCKDIR=$5
 
-
 docker build docker -t test_docker --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g)
 
 docker run --rm -it -v $PWD:/work -v $OUTDIR:/out -w /work test_docker $KEYJSON $URLS $BUCKET $BUCKDIR $RM
 
 # first you must have a service account for the bucket you aim to connect to
-# and generate a creential file with
+# and generate a credential file with
 # gcloud iam service-accounts keys create 
 
 # ./start.sh monument2_key.json small.txt /media/data/Downloads/ monument_bucket  Carr
