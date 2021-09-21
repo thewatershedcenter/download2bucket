dec73f2420ba3be915c37b2f98683dcca5aef185
michael <michaelhuggins@protonmail.com>
2021-09-21 09:01:28 -0700

    modified entine_file and netwine_start to make an ept that also gets moved to buckdir

--- entwine_file.sh
+++ entwine_file.sh
@@ -5,27 +5,36 @@ KEYJSON=$1
 URLS=$2
 BUCKET=$3
 BUCKDIR=$4
-RM=$5
-AG=$5
+EPSG=$5
+RM=$6
+AG=$7
 
 # authorize gcloud account
 gcloud auth activate-service-account --key-file $KEYJSON
 
 # make a directory for downloaded files within outpath
-mkdir -p /out/$BUCKDIR
+mkdir -p /out/$BUCKDIR/las
 
 # make afunction to handle curling and saveing
 curling_func(){
     url=$1
     bkdir=$2
     f="$(echo $url | rev | cut -d"/" -f1 | rev)"
-    curl -L -s -o /out/$bkdir/$f $url
+    curl -L -s -o /out/$bkdir/las/$f $url
     #[[ $AG = remove ]]; gsutil cp /out/$BUCKDIR/$f gs://$BUCKET/$BUCKDIR/ && rm /out/$BUCKDIR/$f
 }
 export -f curling_func
 
-# curl files in ||
+# curl files in || (note the way BUCKDIR is passed is wierd, the function puts it in between  /out and /las)
 cat $URLS | parallel --bar curling_func {1} $BUCKDIR
+
+# how many cores have we?
+NCORES=$(grep -c ^processor /proc/cpuinfo)
+
+# make an entwine dir
+mkdir -p /out/$BUCKDIR/entwine
+
+entwine build -i /out/$BUCKDIR/las -o /out/$BUCKDIR/entwine --srs $EPSG -t $NCORES
 
 # copy files to gcloud bucket
 gsutil -m cp -r /out/$BUCKDIR gs://$BUCKET
---
+++ entwine_start.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+RM=do_not_remove
+AG=do_not_remove
+
+while getopts "hrak:u:o:b:d:" flag; do
+case "$flag" in
+    h)  
+        echo "SYNOPSIS"  
+        echo "     ./start [options] -k key_file -u list_of_urls -o output_dir -b bucket -d bucket_dir "
+        echo "DESCRIPTION"
+        echo "     Downloads files from list"
+        echo "     Copies them to gcloud bucket"
+        echo "        "
+        echo "     options:"
+        echo "        "
+        echo "        -h - display help"
+        echo "        "
+        echo "        -r - if set local files will be deleted after being"
+        echo "        colpied to the bucket"
+        echo "        "
+        echo "        -a - if set each file will be delected as soon as it is"
+        echo "        copied to the bucket. Intended for use with a large"
+        echo "        number of files that may require more disk space"
+        echo "        than available on local machine. Probably slower."
+        echo "        "        
+        echo "     arguments:"
+        echo "        "
+        echo "        -k [key_file] - gcloud service account key json file, see"
+        echo "        https://cloud.google.com/docs/authentication/getting-started,"
+        echo "        or use gcloud iam service-accounts keys create"
+        echo "        " 
+        echo "        -u [list_of_urls] - list of urls to be downloaded, one per line."
+        echo "        "
+        echo "        -o [output_dir] - local directory that will be mounted by the"
+        echo "        container and used to store downloaded files"
+        echo "        "
+        echo "        -b [bucket] - name of gcloud bucket to which files should be"
+        echo "        transfered"
+        echo "        "
+        echo "        -d [bucket_dir] - name of directory which will be created (if"
+        echo "        it does not exist)"
+        echo "        in bucket to store files. (It is my understanding that google"
+        echo "        buckets do not actually have directories, they are flat,"
+        echo "        but the files will appear in the bucket with names"
+        echo "        as though they were in directories)"
+        exit 0
+        ;;
+    r)  RM=remove;;
+    a)  echo "-a is not yet implemented"
+        exit 0
+        AG=remove
+        RM=remove;;
+    k)  KEYJSON=$OPTARG;;
+    u)  URLS=$OPTARG;;
+    o)  OUTDIR=$OPTARG;;    
+    b)  BUCKET=$OPTARG;;
+    d)  BUCKDIR=$OPTARG;;
+    e)  EPSG=$OPTARG;;
+esac
+done
+
+docker build entwine_docker -t gcloud_ept_docker --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g)
+
+docker run --rm -it -v $PWD:/work -v $OUTDIR:/out -w /work gcloud_ept_docker $KEYJSON $URLS $BUCKET $BUCKDIR $EPSG $RM $AG
+
+# first you must have a service account for the bucket you aim to connect to
+# and generate a credential file with
+# gcloud iam service-accounts keys create 
+
+# ./start.sh -r -k monument2_key.json -u small.txt -o /media/data/Downloads/ -b monument_bucket -d Carr_ 
+# ./start.sh -r -k monument2_key.json -u carr_downloadlist_lidar.txt -o /media/data/Downloads/ -b monument_bucket -d carr_lidar 
