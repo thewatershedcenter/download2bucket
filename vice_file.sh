8b83d4ab36ca0f7ce6eaa2c3217d521b31fe5d72
michael <michaelhuggins@protonmail.com>
2021-09-24 09:27:41 -0700

    In vice_docker ditched user in  Dockerfile.  Changed workdir to /root/work. Removed the RM and AG flags alltogether

--- vice_file.sh
+++ vice_file.sh
@@ -5,14 +5,12 @@ URLS=$2
 BUCKET=$3
 BUCKDIR=$4
 EPSG=$5
-RM=$6
-AG=$7
 
 # authorize gcloud account
 gcloud auth activate-service-account --key-file $KEYJSON
 
 # make a directory for downloaded las files
-LAS=/home/user/work/$BUCKDIR/las
+LAS=/root/work/$BUCKDIR/las
 mkdir -p $LAS
 
 # make afunction to handle curling and saveing
@@ -21,7 +19,6 @@ curling_func(){
     bkdir=$2
     f="$(echo $url | rev | cut -d"/" -f1 | rev)"
     curl -L -s -o /out/$bkdir/las/$f $url
-    #[[ $AG = remove ]]; gsutil cp /out/$BUCKDIR/$f gs://$BUCKET/$BUCKDIR/ && rm /out/$BUCKDIR/$f
 }
 export -f curling_func
 
@@ -32,16 +29,13 @@ cat $URLS | parallel --bar curling_func {1} $BUCKDIR
 NCORES=$(grep -c ^processor /proc/cpuinfo)
 
 # make an entwine dir
-ENTWINE=/home/user/work/$BUCKDIR/entwine
+ENTWINE=/root/work/$BUCKDIR/entwine
 mkdir -p $ENTWINE
 
 entwine build -i $LAS -o $ENTWINE --srs $EPSG -t $NCORES
 
 # copy files to gcloud bucket
-gsutil -m cp -r /home/user/work/$BUCKDIR gs://$BUCKET
+gsutil -m cp -r /root/work/$BUCKDIR gs://$BUCKET
 
-# if -r or -a flag was set in start remove files or dir
-if [[ $RM = remove ]]
-then
-    rm -r /home/user/work/$BUCKDIR
-fi
+
+
